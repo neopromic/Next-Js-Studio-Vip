a4b515e67d26e23c7dda900dfbcc0da5363815ca
Luís Miguel <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-04 09:10:53 -0700

    feat: improve ui in ProductsPage

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,175 +1,161 @@
-import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
-} from "@/components/ui/select";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { SparkleIcon, Sparkles, StarsIcon } from "lucide-react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import SearchInputWithIcon from "@/components/Input-Icon";
-import { Mail } from "lucide-react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { StarsIcon } from "lucide-react";
 import Link from "next/link";
 export default function PaginaDeProdutos() {
-    return (
-        <>
-          <section>
-
-        <h1 className="flex justify-center font-bold text-5xl mt-5">
-            STUDIO POR PERTO
+  return (
+    <>
+      <section className="px-6 py-4">
+        <h1 className="font-bold w-full text-center mt-4 text-3xl tracking-tighter md:text-5xl md:text-start">
+          Estudios por perto
         </h1>
+        <p className="text-muted-foreground leading-relaxed text-center md:text-xl md:text-start mt-2">
+          Que tal marcar com uma dos profissionais disponiveis em nossa
+          plataforma.
+        </p>
+        <section className="grid grid-cols-1 place-items-center md:grid-cols-4 justify-center gap-4">
+          <Card className="w-80 h-max  mt-12 hover:size-11/12 hover:transition-all hover:size-11/12 hover:transition-all">
+            <CardHeader>
+              <Image
+                height={320}
+                width={200}
+                className="w-full object-cover aspect-square rounded-md"
+                alt="Tatuagem tato servicos agendamentos"
+                src={"/tatto.jpg"}
+              />
+            </CardHeader>
+            <CardContent className="flex space-y-4">
+              <div>
+                <h1 className="font-bold">Dhan - tatto</h1>
+                <h1>Jardim Belval - Barueri</h1>
+                <Button className="mt-5">Marcar</Button>
+              </div>
+              <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
+                <StarsIcon size={14} />
+                <p>5.0</p>
+              </div>
+            </CardContent>
+          </Card>
 
-            <section className="flex justify-center gap-10">
-            <Card className="w-fit  mt-12">
-                      <CardHeader>
-                        <Image
-                          height={320}
-                          width={190}
-                          alt="Tatuagem tato servicos agendamentos"
-                          src={"/tatto.jpg"}
-                        />
-                      </CardHeader>
-                      <CardContent className="flex space-y-4">
-                        <div>
-                          <h1 className="font-bold">Dhan - tatto</h1>
-                          <h1>Jardim Belval - Barueri</h1>
-                          <Button className="mt-5">Marcar</Button>
-                        </div>
-                        <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
-                          <StarsIcon size={14} />
-                          <p>5.0</p>
-                        </div>
-                      </CardContent>
-                    </Card>
+          <Card className="w-80 h-max  mt-12 hover:size-11/12 hover:transition-all">
+            <CardHeader>
+              <Image
+                height={320}
+                width={200}
+                className="w-full object-cover aspect-square rounded-md"
+                alt="Tatuagem tato servicos agendamentos"
+                src={"/tatto2.jpg"}
+              />
+            </CardHeader>
+            <CardContent className="flex space-y-4">
+              <div>
+                <h1 className="font-bold">Alex - Cabeleleiro</h1>
+                <h1>LeoPoldina - Osasco</h1>
+                <Button className="mt-5">Marcar</Button>
+              </div>
+              <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
+                <StarsIcon size={14} />
+                <p>5.0</p>
+              </div>
+            </CardContent>
+          </Card>
 
-                    <Card className="w-fit  mt-12">
-                      <CardHeader>
-                        <Image
-                          height={320}
-                          width={190}
-                          alt="Tatuagem tato servicos agendamentos"
-                          src={"/tatto2.jpg"}
-                        />
-                      </CardHeader>
-                      <CardContent className="flex space-y-4">
-                        <div>
-                          <h1 className="font-bold">Alex - Cabeleleiro</h1>
-                          <h1>LeoPoldina - Osasco</h1>
-                          <Button className="mt-5">Marcar</Button>
-                        </div>
-                        <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
-                          <StarsIcon size={14} />
-                          <p>5.0</p>
-                        </div>
-                      </CardContent>
-                    </Card>
+          <Card className="w-80 h-max  mt-12 hover:size-11/12 hover:transition-all">
+            <CardHeader>
+              <Image
+                height={320}
+                width={200}
+                className="w-full object-cover aspect-square rounded-md"
+                alt="Tatuagem tato servicos agendamentos"
+                src={"/spa210.jpg"}
+              />
+            </CardHeader>
+            <CardContent className="flex space-y-4">
+              <div>
+                <h1 className="font-bold ">Helena - Spa</h1>
+                <h1>Jandira</h1>
+                <Button className="mt-5">Marcar</Button>
+              </div>
+              <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
+                <StarsIcon size={14} />
+                <p>5.0</p>
+              </div>
+            </CardContent>
+          </Card>
+        </section>
 
-                    <Card className="w-fit  mt-12">
-                      <CardHeader>
-                        <Image
-                          height={320}
-                          width={190}
-                          alt="Tatuagem tato servicos agendamentos"
-                          src={"/spa210.jpg"}
-                        />
-                      </CardHeader>
-                      <CardContent className="flex space-y-4">
-                        <div>
-                          <h1 className="font-bold ">Helena - Spa</h1>
-                          <h1>Jandira</h1>
-                          <Button className="mt-5">Marcar</Button>
-                        </div>
-                        <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
-                          <StarsIcon size={14} />
-                          <p>5.0</p>
-                        </div>
-                      </CardContent>
-                    </Card>
-            </section>
+        <section className="grid grid-cols-1 place-items-center md:grid-cols-4 justify-center gap-2">
+          <Card className="w-80 h-max  mt-12 hover:size-11/12 hover:transition-all">
+            <CardHeader>
+              <Image
+                height={320}
+                width={200}
+                className="w-full object-cover aspect-square rounded-md"
+                alt="Tatuagem tato servicos agendamentos"
+                src={"/tatto.jpg"}
+              />
+            </CardHeader>
+            <CardContent className="flex space-y-4">
+              <div>
+                <h1 className="font-bold">Dhan - tatto</h1>
+                <h1>Jardim Belval - Barueri</h1>
+                <Button className="mt-5">Marcar</Button>
+              </div>
+              <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
+                <StarsIcon size={14} />
+                <p>5.0</p>
+              </div>
+            </CardContent>
+          </Card>
 
-            <section className="flex justify-center gap-10">
+          <Card className="w-80 h-max  mt-12 hover:size-11/12 hover:transition-all">
+            <CardHeader>
+              <Image
+                height={320}
+                width={200}
+                className="w-full object-cover aspect-square rounded-md"
+                alt="Tatuagem tato servicos agendamentos"
+                src={"/tatto.jpg"}
+              />
+            </CardHeader>
+            <CardContent className="flex space-y-4">
+              <div>
+                <h1 className="font-bold">Dhan - tatto</h1>
+                <h1>Jardim Belval - Barueri</h1>
+                <Button className="mt-5">Marcar</Button>
+              </div>
+              <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
+                <StarsIcon size={14} />
+                <p>5.0</p>
+              </div>
+            </CardContent>
+          </Card>
 
-            <Card className="w-fit  mt-12">
-                      <CardHeader>
-                        <Image
-                          height={320}
-                          width={190}
-                          alt="Tatuagem tato servicos agendamentos"
-                          src={"/tatto.jpg"}
-                        />
-                      </CardHeader>
-                      <CardContent className="flex space-y-4">
-                        <div>
-                          <h1 className="font-bold">Dhan - tatto</h1>
-                          <h1>Jardim Belval - Barueri</h1>
-                          <Button className="mt-5">Marcar</Button>
-                        </div>
-                        <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
-                          <StarsIcon size={14} />
-                          <p>5.0</p>
-                        </div>
-                      </CardContent>
-                    </Card>
-
-                    <Card className="w-fit  mt-12">
-                      <CardHeader>
-                        <Image
-                          height={320}
-                          width={190}
-                          alt="Tatuagem tato servicos agendamentos"
-                          src={"/tatto.jpg"}
-                        />
-                      </CardHeader>
-                      <CardContent className="flex space-y-4">
-                        <div>
-                          <h1 className="font-bold">Dhan - tatto</h1>
-                          <h1>Jardim Belval - Barueri</h1>
-                          <Button className="mt-5">Marcar</Button>
-                        </div>
-                        <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
-                          <StarsIcon size={14} />
-                          <p>5.0</p>
-                        </div>
-                      </CardContent>
-                    </Card>
-
-                    <Card className="w-fit  mt-12">
-                      <CardHeader>
-                        <Image
-                          height={320}
-                          width={190}
-                          alt="Tatuagem tato servicos agendamentos"
-                          src={"/tatto.jpg"}
-                        />
-                      </CardHeader>
-                      <CardContent className="flex space-y-4">
-                        <div>
-                          <h1 className="font-bold">Dhan - tatto</h1>
-                          <h1>Jardim Belval - Barueri</h1>
-                          <Button className="mt-5">Marcar</Button>
-                        </div>
-                        <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
-                          <StarsIcon size={14} />
-                          <p>5.0</p>
-                        </div>
-                      </CardContent>
-                    </Card>
-
-            </section>
-
-          </section>
-
-
-        </>
-    )
+          <Card className="w-80 h-max  mt-12 hover:size-11/12 hover:transition-all">
+            <CardHeader>
+              <Image
+                height={320}
+                width={200}
+                className="w-full object-cover aspect-square rounded-md"
+                alt="Tatuagem tato servicos agendamentos"
+                src={"/tatto.jpg"}
+              />
+            </CardHeader>
+            <CardContent className="flex space-y-4">
+              <div>
+                <h1 className="font-bold">Dhan - tatto</h1>
+                <h1>Jardim Belval - Barueri</h1>
+                <Button className="mt-5">Marcar</Button>
+              </div>
+              <div className="flex bg-yellow-400 h-fit rounded-full px-2 items-center gap-1 -translate-y-4">
+                <StarsIcon size={14} />
+                <p>5.0</p>
+              </div>
+            </CardContent>
+          </Card>
+        </section>
+      </section>
+    </>
+  );
 }
